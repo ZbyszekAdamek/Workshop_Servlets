dc7650163b02b9f9129e76fedf57b72dfffb2cf2
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-06-18 13:49:36 +0200

    git add.

--- src/main/java/pl/coderslab/users/UserList.java
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -11,6 +11,7 @@ public class UserList extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/users/list.jsp")
                 .forward(request, response);
+
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
